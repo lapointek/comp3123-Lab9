201af9edb5c3e1d5f8ed4ed2dfaf061ee3dab88e
Kevin Lapointe <156107790+lapointek@users.noreply.github.com>
2024-11-03 17:58:40 -0500

    Lab9-React-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+            </header>
+            <body class="description">
+                <h1>Welcome to Fullstack Development - I</h1>
+                <h2>React JS Programming Week09 Lab exercise</h2>
+                <h3>101452430</h3>
+                <h4>Kevin Lapointe</h4>
+                <h5>George Brown College, Toronto</h5>
+            </body>
+        </div>
+    );
 }
 
 export default App;
